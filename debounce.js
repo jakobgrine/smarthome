09cb221687f2d61c95cdc9f53a2fca253988c202
jakobgrine <18680782+jakobgrine@users.noreply.github.com>
2020-11-10 11:29:13 +0100

    Fix indentation

--- debounce.js
+++ debounce.js
@@ -1,14 +1,14 @@
 exports.debounce = (func, delay) => {
     let timeout;
-	return function() {
-	    const context = this;
+    return function() {
+        const context = this;
         const args = arguments;
 
-		clearTimeout(timeout);
-		timeout = setTimeout(() => {
-			timeout = null;
-			func.apply(context, args);
-		}, delay);
-	};
+        clearTimeout(timeout);
+        timeout = setTimeout(() => {
+            timeout = null;
+            func.apply(context, args);
+        }, delay);
+    };
 };
 
